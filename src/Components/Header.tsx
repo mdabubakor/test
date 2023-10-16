8c8dff8b6375801e9cd89e283900cf72808be0f1
abubakar460 <mdsimul5060@gmail.com>
2023-10-16 12:39:12 -0700

    datteri

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-center gap-10 bg-green-500'>
+      <div>Home</div>
+      <div>About</div>
+      <div>services</div>
+      <div>Contact</div>
+    </div>
+  )
+}
+
+export default Header
